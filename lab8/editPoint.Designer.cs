761594981fe84d846f7be8669cc3256d27429499
Khang Lam Hoang <llamhoangkhang00000@gmail.com>
2024-03-10 18:50:42 -0700

    editPoint form done

---
+++ lab8/editPoint.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace lab8
+{
+    partial class editPoint
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvClasses = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtClass = new System.Windows.Forms.TextBox();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClasses)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvClasses
+            // 
+            this.dgvClasses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClasses.Location = new System.Drawing.Point(52, 103);
+            this.dgvClasses.Name = "dgvClasses";
+            this.dgvClasses.Size = new System.Drawing.Size(624, 313);
+            this.dgvClasses.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Class";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Subject";
+            // 
+            // txtClass
+            // 
+            this.txtClass.Location = new System.Drawing.Point(106, 26);
+            this.txtClass.Name = "txtClass";
+            this.txtClass.ReadOnly = true;
+            this.txtClass.Size = new System.Drawing.Size(173, 20);
+            this.txtClass.TabIndex = 3;
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(106, 59);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.ReadOnly = true;
+            this.txtSubject.Size = new System.Drawing.Size(570, 20);
+            this.txtSubject.TabIndex = 4;
+            // 
+            // editPoint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 452);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.txtClass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvClasses);
+            this.Name = "editPoint";
+            this.Text = "Transcript";
+            this.Load += new System.EventHandler(this.editPoint_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClasses)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvClasses;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtClass;
+        private System.Windows.Forms.TextBox txtSubject;
+    }
+}
